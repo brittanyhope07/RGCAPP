d561d0f41eac3275617504ef214ec1b45aa6df7c
Brittany McMahan <brittanyhope07@live.com>
2022-02-23 15:08:41 -0500

    removed send grid api

--- Email Services/EmailService.cs
+++ Email Services/EmailService.cs
@@ -23,7 +23,7 @@ namespace RGCAPP.Email_Services
         {
             var apiKey = _configuration["SendGridAPIKey"];
             var client = new SendGridClient(apiKey);
-            var from = new EmailAddress("bhm.rgc@com", "Brittany");
+            var from = new EmailAddress("brittanyhope07@gmail.com", "Brittany");
             var to = new EmailAddress("toEmail");
             var msg = MailHelper.CreateSingleEmail(from, to, subject, content, content);
             var response = await client.SendEmailAsync(msg);
--- Startup.cs
+++ Startup.cs
@@ -35,7 +35,13 @@ namespace RGCAPP
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
+
+            services.AddScoped<IUserService, UserService>();
+            services.AddTransient<IMailService, SendGridMailService>();
+            services.AddControllers();
+
         }
+
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
